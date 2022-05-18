515477092decc2f52824cf59afdad6c188df54f4
kadimitra <kadimitra@ece.auth.gr>
2022-05-18 18:07:04 +0200

    add git ignore

--- scripts/train_non_iid_50.sh
+++ scripts/train_non_iid_50.sh
@@ -1,9 +1,9 @@
-python3 ../main.py --gpu $1 \
+python3 ../main.py --gpu 1 \
 					--work-type train \
 					--model fedweit \
 					--task non_iid_50 \
 					--gpu-mem-multiplier 9 \
-					--num-rounds 20 \
+					--num-rounds 1 \
 					--num-epochs 1 \
 					--batch-size 100 \
 					--seed 777 \
